c97e284969230d17b6a2b44fda985fb0cc6d2eec
Ben Zimmer <zero@zeroathome.de>
2011-03-27 15:07:15 +0200

    adds timestamps to couchdb queries

--- lib/db/couchdbstats.library.bash
+++ lib/db/couchdbstats.library.bash
@@ -8,15 +8,16 @@
 zresql() {
 
     couchsend='curl -s -o /dev/null -X PUT http://127.0.0.1:5984/zre/'`python  -c "import uuid; print uuid.uuid1()"`' -d'
+    timestamp=`date "+%Y%m%dT%H%M%S"`
     case "$1" in
-        kill) $couchsend '{"doc_type":"kill", "side":"'$2'", "kills": '$3'}' ;;
-        death) $couchsend '{"doc_type":"death", "side":"'$2'"}' ;;
-        born) $couchsend '{"doc_type":"born", "side":"'$2'"}' ;;
-        win) $couchsend '{"doc_type":"win", "side":"'$2'", "survivors":'$3'}' ;;
-        support) $couchsend '{"doc_type":"support", "side":"'$2'", "size":'$3'}' ;;
-        weather) $couchsend '{"doc_type":"weather", "weather":"'$2'"}' ;;
-        stats) $couchsend '{"doc_type":"stats", "side":"'$2'", "stat":"'$3'", "statchange":'$4', "type":"'$5'"}' ;;
-        buildings) $couchsend '{"doc_type":"buildings", "side":"'$2'", "building":"'$3'", "size":'$4', "finish":'$5'}' ;;
+        born) $couchsend '{"doc_type":"born", "side":"'$2'", "date":"'${timestamp}'"}' ;;
+        buildings) $couchsend '{"doc_type":"buildings", "side":"'$2'", "building":"'$3'", "size":'$4', "finish":'$5', "date":"'${timestamp}'"}' ;;
+        death) $couchsend '{"doc_type":"death", "side":"'$2'", "date":"'${timestamp}'"}' ;;
+        kill) echo '{"doc_type":"kill", "side":"'$2'", "kills":'$3', "date":"'${timestamp}'", "date":"'${timestamp}'"}' ;;
+        support) $couchsend '{"doc_type":"support", "side":"'$2'", "size":'$3', "date":"'${timestamp}'"}' ;;
+        stats) $couchsend '{"doc_type":"stats", "side":"'$2'", "stat":"'$3'", "statchange":'$4', "type":"'$5'", "date":"'${timestamp}'"}' ;;
+        weather) $couchsend '{"doc_type":"weather", "weather":"'$2'", "date":"'${timestamp}'"}' ;;
+        win) $couchsend '{"doc_type":"win", "side":"'$2'", "survivors":'$3', "date":"'${timestamp}'"}' ;;
         *) echo "Error in Usage:"; echo "Options: born | death | kill | win | support | weather | stats | buildings" ;;
     esac
 }
